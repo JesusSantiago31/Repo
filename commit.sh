9d9819eca0d31d1784c427fa4e8c781ef47c1d51
JesusSantiago31 <jesussantiagocr31@gmail.com>
2025-09-18 10:40:33 -0600

    Basic

--- commit.sh
+++ commit.sh
@@ -23,7 +23,7 @@ start_epoch=$(date -d "$START_DATE" +%s)
 end_epoch=$(date -d "$END_DATE" +%s)
 current_epoch=$start_epoch
 
-while [ "$current_epoch" -le "$end_epoch" ]; s
+while [ "$current_epoch" -le "$end_epoch" ]; 
   current=$(date -d "@$current_epoch" +%Y-%m-%d)
 
   for((i=1; i<=COMMITS_PER_DAY; i++));do
